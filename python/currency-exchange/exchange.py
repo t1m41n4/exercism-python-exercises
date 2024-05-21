ac82c8b3c9e192dbc614610ac7d880c09e7d12d4
tim <timmaina64@gmail.com>
2024-05-21 22:38:23 +0300

    Implement currency exchange numbers exercise

--- python/currency-exchange/exchange.py
+++ python/currency-exchange/exchange.py
@@ -15,9 +15,7 @@ def exchange_money(budget, exchange_rate):
     :return: float - exchanged value of the foreign currency you can receive.
     """
 
-    budget = 1000
-    exchange_rate = 100
-    exchanged_value = budget/exchange_rate
+    return budget/exchange_rate
 
 
 def get_change(budget, exchanging_value):
@@ -28,7 +26,7 @@ def get_change(budget, exchanging_value):
     :return: float - amount left of your starting currency after exchanging.
     """
 
-    pass
+    return budget - exchanging_value
 
 
 def get_value_of_bills(denomination, number_of_bills):
@@ -39,7 +37,7 @@ def get_value_of_bills(denomination, number_of_bills):
     :return: int - calculated value of the bills.
     """
 
-    pass
+    return denomination * number_of_bills
 
 
 def get_number_of_bills(amount, denomination):
@@ -50,7 +48,7 @@ def get_number_of_bills(amount, denomination):
     :return: int - number of bills that can be obtained from the amount.
     """
 
-    pass
+    return amount // denomination
 
 
 def get_leftover_of_bills(amount, denomination):
@@ -61,7 +59,7 @@ def get_leftover_of_bills(amount, denomination):
     :return: float - the amount that is "leftover", given the current denomination.
     """
 
-    pass
+    return amount % denomination
 
 
 def exchangeable_value(budget, exchange_rate, spread, denomination):
@@ -74,4 +72,4 @@ def exchangeable_value(budget, exchange_rate, spread, denomination):
     :return: int - maximum value you can get.
     """
 
-    pass
+    return int(budget / ((spread / 100) * exchange_rate + exchange_rate) // denomination * denomination)
