a280b1d557f084cfcad70a91423ec12808ea8daa
tim <timmaina64@gmail.com>
2024-05-22 22:20:42 +0300

    Implement dart exercise

--- python/darts/darts.py
+++ python/darts/darts.py
@@ -1,2 +1,11 @@
 def score(x, y):
-    pass
+    distance_squared = x**2 + y**2
+
+    if distance_squared <= 1:
+        return 10
+    elif distance_squared <= 25:
+        return 5
+    elif distance_squared <= 100:
+        return 1
+    else:
+        return 0
