359bbe6eefb3878f9ca09eda73c42d9ee0e39345
tim <timmaina64@gmail.com>
2024-05-29 22:19:20 +0300

    Implement collatz conjecture numbers exercise

--- python/collatz-conjecture/collatz_conjecture.py
+++ python/collatz-conjecture/collatz_conjecture.py
@@ -1,2 +1,13 @@
 def steps(number):
-    pass
+    if number <= 0:
+        raise ValueError("Only positive integers are allowed")
+
+    step_count = 0
+    while number != 1:
+        if number % 2 == 0:
+            number //= 2
+        else:
+            number = 3 * number + 1
+        step_count += 1
+
+    return step_count
