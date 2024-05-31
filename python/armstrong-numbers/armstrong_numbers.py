ec0cb9f39b24de28456fe23136fc739d53785872
tim <timmaina64@gmail.com>
2024-05-31 23:28:29 +0300

    Implement armstrong numbers exercise

--- python/armstrong-numbers/armstrong_numbers.py
+++ python/armstrong-numbers/armstrong_numbers.py
@@ -1,2 +1,5 @@
 def is_armstrong_number(number):
-    pass
+    digits = str(number)
+    num_digits = len(digits)
+    armstrong_sum = sum(int(digit) ** num_digits for digit in digits)
+    return armstrong_sum == number
