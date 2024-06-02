5895c98a939e616efa6158db4eadcbbf6ef173c1
tim <timmaina64@gmail.com>
2024-06-02 11:14:44 +0300

    Implement bob conditionals exercise

--- python/bob/bob.py
+++ python/bob/bob.py
@@ -1,2 +1,17 @@
 def response(hey_bob):
-    pass
+    hey_bob = hey_bob.strip()
+
+    if not hey_bob:
+        return "Fine. Be that way!"
+
+    is_question = hey_bob.endswith('?')
+    is_shouting = hey_bob.isupper() and any(c.isalpha() for c in hey_bob)
+
+    if is_shouting and is_question:
+        return "Calm down, I know what I'm doing!"
+    elif is_shouting:
+        return "Whoa, chill out!"
+    elif is_question:
+        return "Sure."
+    else:
+        return "Whatever."
