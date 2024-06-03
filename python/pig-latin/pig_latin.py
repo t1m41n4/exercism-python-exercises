f7224a91c032469c3d3940710d93839f0bb86969
tim <timmaina64@gmail.com>
2024-06-03 23:09:42 +0300

    Implement pig_latin conditionals exercise

--- python/pig-latin/pig_latin.py
+++ python/pig-latin/pig_latin.py
@@ -1,2 +1,15 @@
 def translate(text):
-    pass
+    def translate_word(word):
+        vowels = "aeiou"
+        if word[0] in vowels or word[:2] in ["xr", "yt"]:
+            return word + "ay"
+
+        for i in range(len(word)):
+            if word[i] in vowels or (word[i] == "y" and i != 0):
+                # Special case for "qu"
+                if word[i-1:i+1] == "qu":
+                    return word[i+1:] + word[:i+1] + "ay"
+                return word[i:] + word[:i] + "ay"
+        return word + "ay"
+
+    return ' '.join(translate_word(word) for word in text.split())
