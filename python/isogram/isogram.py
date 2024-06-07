255778d8da2f4715cba43dca50d68ae3fd58a355
tim <timmaina64@gmail.com>
2024-06-07 23:24:09 +0300

    Implement isogram strings exercise

--- python/isogram/isogram.py
+++ python/isogram/isogram.py
@@ -1,2 +1,13 @@
 def is_isogram(string):
-    pass
+    string = string.lower()
+    seen = set()
+
+    for char in string:
+        if char == "-" or char == " ":
+            continue
+        if char in seen:
+            return False
+
+        seen.add(char)
+
+    return True
