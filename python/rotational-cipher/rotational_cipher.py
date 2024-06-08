ad8d34823d3534570f41996cda52af763b995d6e
tim <timmaina64@gmail.com>
2024-06-08 23:30:10 +0300

    Implement rotational-cipher strings exercise

--- python/rotational-cipher/rotational_cipher.py
+++ python/rotational-cipher/rotational_cipher.py
@@ -1,2 +1,13 @@
 def rotate(text, key):
-    pass
+    result = []
+
+    for char in text:
+        if 'a' <= char <= 'z':  # Check if character is a lowercase letter
+            new_char = chr((ord(char) - ord('a') + key) % 26 + ord('a'))
+        elif 'A' <= char <= 'Z':  # Check if character is an uppercase letter
+            new_char = chr((ord(char) - ord('A') + key) % 26 + ord('A'))
+        else:  # Non-alphabetic characters remain the same
+            new_char = char
+        result.append(new_char)
+
+    return ''.join(result)
