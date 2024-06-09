99cc91bac8a0d6c9277feb8e6df21afd50dff011
tim <timmaina64@gmail.com>
2024-06-09 23:13:36 +0300

    Implement pangram strings exercise

--- python/pangram/pangram.py
+++ python/pangram/pangram.py
@@ -1,2 +1,8 @@
 def is_pangram(sentence):
-    pass
+    unique_letters = set()
+
+    for char in sentence.lower():
+        if 'a' <= char <= 'z':
+            unique_letters.add(char)
+
+    return len(unique_letters) == 26
