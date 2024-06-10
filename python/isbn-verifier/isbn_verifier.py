b1da5fb4fffcec58d7942133f8cde2661f4509de
tim <timmaina64@gmail.com>
2024-06-10 23:16:48 +0300

    Implement isbn-verifier strings exercise

--- python/isbn-verifier/isbn_verifier.py
+++ python/isbn-verifier/isbn_verifier.py
@@ -1,2 +1,17 @@
 def is_valid(isbn):
-    pass
+    isbn = isbn.replace("-", "")
+
+    if len(isbn) != 10:
+        return False
+
+    total_sum = 0
+
+    for i, char in enumerate(isbn):
+        if char == 'X' and i == 9:
+            total_sum += 10 * (i + 1)
+        elif char.isdigit():
+            total_sum += int(char) * (i + 1)
+        else:
+            return False
+
+    return total_sum % 11 == 0
