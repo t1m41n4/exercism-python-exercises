fa6e836b8258801644c1e001bd74dac18eb0f653
tim <timmaina64@gmail.com>
2024-06-11 21:37:18 +0300

    Implement perfect-numbers comparisons exercise

--- python/perfect-numbers/perfect_numbers.py
+++ python/perfect-numbers/perfect_numbers.py
@@ -4,4 +4,14 @@ def classify(number):
     :param number: int a positive integer
     :return: str the classification of the input integer
     """
-    pass
+    if number <= 0:
+        raise ValueError("Classification is only possible for positive integers.")
+
+    sum_of_divisors = sum(i for i in range(1, number) if number % i == 0)
+
+    if sum_of_divisors == number:
+        return "perfect"
+    elif sum_of_divisors > number:
+        return "abundant"
+    else:
+        return "deficient"
