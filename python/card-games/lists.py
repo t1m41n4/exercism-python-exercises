30ccd5210f85c84ec91a2f8523f5a91beb0e9d68
tim <timmaina64@gmail.com>
2024-06-12 22:24:14 +0300

    Implement card-games list exercise

--- python/card-games/lists.py
+++ python/card-games/lists.py
@@ -10,8 +10,7 @@ def get_rounds(number):
     :param number: int - current round number.
     :return: list - current round and the two that follow.
     """
-
-    pass
+    return [number, number + 1, number + 2]
 
 
 def concatenate_rounds(rounds_1, rounds_2):
@@ -21,8 +20,7 @@ def concatenate_rounds(rounds_1, rounds_2):
     :param rounds_2: list - second set of rounds played.
     :return: list - all rounds played.
     """
-
-    pass
+    return rounds_1 + rounds_2
 
 
 def list_contains_round(rounds, number):
@@ -32,8 +30,7 @@ def list_contains_round(rounds, number):
     :param number: int - round number.
     :return: bool - was the round played?
     """
-
-    pass
+    return number in rounds
 
 
 def card_average(hand):
@@ -42,8 +39,7 @@ def card_average(hand):
     :param hand: list - cards in hand.
     :return: float - average value of the cards in the hand.
     """
-
-    pass
+    return sum(hand) / len(hand)
 
 
 def approx_average_is_average(hand):
@@ -52,8 +48,11 @@ def approx_average_is_average(hand):
     :param hand: list - cards in hand.
     :return: bool - does one of the approximate averages equal the `true average`?
     """
+    true_average = card_average(hand)
+    approx_average = (hand[0] + hand[-1]) / 2
+    middle_card = hand[len(hand) // 2]
 
-    pass
+    return true_average in {approx_average, middle_card}
 
 
 def average_even_is_average_odd(hand):
@@ -62,8 +61,10 @@ def average_even_is_average_odd(hand):
     :param hand: list - cards in hand.
     :return: bool - are even and odd averages equal?
     """
+    even_cards = hand[0::2]
+    odd_cards = hand[1::2]
 
-    pass
+    return card_average(even_cards) == card_average(odd_cards)
 
 
 def maybe_double_last(hand):
@@ -72,5 +73,6 @@ def maybe_double_last(hand):
     :param hand: list - cards in hand.
     :return: list - hand with Jacks (if present) value doubled.
     """
-
-    pass
+    if hand and hand[-1] == 11:
+        hand[-1] *= 2
+    return hand
