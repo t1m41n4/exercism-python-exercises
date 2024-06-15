4d0d539ab37867e414e0348fc63c959e0e1ccea8
tim <timmaina64@gmail.com>
2024-06-15 23:18:04 +0300

    Add resistor-color exercise

---
+++ python/resistor-color/resistor_color.py
@@ -0,0 +1,29 @@
+def color_code(color):
+    color_list = [
+        "black",
+        "brown",
+        "red",
+        "orange",
+        "yellow",
+        "green",
+        "blue",
+        "violet",
+        "grey",
+        "white"
+    ]
+    return color_list.index(color)
+
+
+def colors():
+    return [
+        "black",
+        "brown",
+        "red",
+        "orange",
+        "yellow",
+        "green",
+        "blue",
+        "violet",
+        "grey",
+        "white"
+    ]
