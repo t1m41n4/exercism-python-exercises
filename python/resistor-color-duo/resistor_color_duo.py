a9f158c1d52a56415339904b0a4a7704b9b641f7
tim <timmaina64@gmail.com>
2024-06-18 23:20:41 +0300

    Implement resistor-color-duo exercise

--- python/resistor-color-duo/resistor_color_duo.py
+++ python/resistor-color-duo/resistor_color_duo.py
@@ -1,2 +1,20 @@
 def value(colors):
-    pass
+    color_list = [
+        "black",
+        "brown",
+        "red",
+        "orange",
+        "yellow",
+        "green",
+        "blue",
+        "violet",
+        "grey",
+        "white"
+    ]
+
+    # Get the values of first two colors
+    first_digit = color_list.index(colors[0])
+    second_digit = color_list.index(colors[1])
+
+    # Combine the digits
+    return first_digit * 10 + second_digit
